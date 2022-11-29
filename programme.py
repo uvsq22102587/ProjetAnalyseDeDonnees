aa4713105553439696b8e888056f8c7c1ebc10b1
Benjamin PREHAUD <benjamin.prehaud@ens.uvsq.fr>
2022-11-29 09:04:46 +0100

    suppr emoji

---
+++ programme.py
@@ -0,0 +1,11 @@
+import re
+text= u"Goumin des éléphants joueurs la même fatigue même 😫 #twitter225"
+
+regrex_pattern = re.compile(pattern = "["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags = re.UNICODE)
+
+print(regrex_pattern.sub(r"", text))
